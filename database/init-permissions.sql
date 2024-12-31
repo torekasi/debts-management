5dc6b5e3d64ce1c2679370555211e64a9a545fbf
nefi <nefizon@gmail.com>
2024-12-31 20:00:44 +0800

    Remove unused init-permissions.sql file

--- database/init-permissions.sql
+++
@@ -1,4 +0,0 @@
--- Create default database user and grant permissions
-CREATE USER IF NOT EXISTS 'debt_user'@'%' IDENTIFIED BY 'debt_password';
-GRANT ALL PRIVILEGES ON mydebts.* TO 'debt_user'@'%';
-FLUSH PRIVILEGES;
